a8861b6ce6a247069c6fcf1201dfcb541e727ede
rhathaway <rhathaway@barkleyus.com>
2013-01-20 23:36:37 -0600

    added comments

--- hw1/part5.rb
+++ hw1/part5.rb
@@ -4,8 +4,12 @@ class Class
     attr_name = attr_name.to_s        # make sure it's a string
     attr_reader attr_name             # create the attribute's getter
     attr_reader attr_name+"_history"  # create bar_history getter
+    # below: when the name of the attr is changed,
+    # check if the history array is defined,
+    # if not, create it, update the attr, 
+    # push the new attr value to the history array
     class_eval %Q{
-      def #{attr_name}=(value)
+      def #{attr_name}=(value)                    
         if !defined? @#{attr_name}_history
           @#{attr_name}_history = [@#{attr_name}]
         end
