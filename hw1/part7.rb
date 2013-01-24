2550ce0316de1ea958fde803a165730bb610b2df
Rachel Hathaway <rachel.e.hathaway@gmail.com>
2013-01-23 20:35:38 -0600

    completed part7

--- hw1/part7.rb
+++ hw1/part7.rb
@@ -1,4 +1,30 @@
 class CartesianProduct
+  
+  attr_reader :cp
+  
   include Enumerable
-  # YOUR CODE HERE
+  
+  def initialize(seq1, seq2)
+    @seq1 = seq1
+    @seq2 = seq2
+    @cp = []
+  end
+  
+  def get_cp
+    @seq2.each do |x|
+      @seq1.each do |y|
+        @cp.push([y,x])
+      end
+    end
+    return @cp.empty? ? nil : @cp
+  end
+  
+  def each 
+    @cp = self.get_cp
+    begin
+      @cp.each { |product| yield product }
+    rescue NoMethodError
+    end
+  end  
+    
 end
